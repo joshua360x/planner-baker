4e04bb19f95b4eb9a297d7e610b551076dc02d15
joshua360x <jkw5499@psu.edu>
2022-05-16 20:18:20 -0400

    added entry point for rendering for list

--- src/components/Planner/EntryList.jsx
+++ src/components/Planner/EntryList.jsx
@@ -1,16 +1,18 @@
+import { useEntries } from '../../context/PlannerContext';
 import Entry from './EntryItem';
 import styles from './EntryList.css';
 
 export default function Entries() {
+  const { entries } = useEntries();
   // TODO: Replace with the entries stored in PlannerContext
-  const entries = [
-    {
-      id: 0,
-      title: 'Replace me!',
-      content: '`content` should only be visible on the detail view',
-      date: new Date()
-    },
-  ];
+  // const entries = [
+  //   {
+  //     id: 0,
+  //     title: 'Replace me!',
+  //     content: '`content` should only be visible on the detail view',
+  //     date: new Date()
+  //   },
+  // ];
 
   return (
     <ul className={styles.list}>
